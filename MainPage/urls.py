49ae74e5354fdf5a1abd0de321d6e793d92b4bd2
Konstantin <ektov_konstantin@mail.ru>
2021-02-15 22:01:59 +0300

    moved static and media folders

--- MainPage/urls.py
+++ MainPage/urls.py
@@ -3,13 +3,9 @@ from django.conf.urls import url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from . import views
 
-
-
 urlpatterns = [
-    url(r'/',views.MainPage, name="mainpage"),
-    url(r'/about',views.About, name="about"),
+    url(r'', views.MainPage, name="mainpage"),
+    url(r'about', views.About, name="about"),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
-
-
